69dd00cf90bb1293529a6b779bf83670b2449af3
hamed <hamedmodam1991@gmail.com>
2026-01-07 20:49:37 +0330

    Removing extra test function

--- tests/Feature/StockUploadTest.php
+++ tests/Feature/StockUploadTest.php
@@ -27,19 +27,4 @@ class StockUploadTest extends TestCase
         // Ensure the import job is dispatched
         Queue::assertPushed(ImportEquityExcelJob::class);
     }
-
-    #[\PHPUnit\Framework\Attributes\Test]
-    public function it_uploads_excel_and_dispatches_import_job()
-    {
-        Queue::fake();
-
-        $file = UploadedFile::fake()->create('stocks.xlsx', 100);
-
-        $response = $this->post('/stocks/upload', [
-            'file' => $file,
-        ]);
-
-        $response->assertStatus(302); // Web form redirects
-        Queue::assertPushed(ImportEquityExcelJob::class);
-    }
 }
